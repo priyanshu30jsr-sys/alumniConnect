8de413df0da1d133ecf884ceb186ceaab0ff15ac
Priyanshu Singh <priyanshu30jsr@gmail.com>
2026-07-05 10:35:33 +0530

    chore: inject global axios interceptor to redirect local endpoints to render

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import axios from 'axios';
+
+const LIVE_BACKEND_URL = 'https://alumniconnect-386v.onrender.com'; 
+
+
+axios.interceptors.request.use((config) => {
+  if (config.url && config.url.startsWith('http://localhost:5000')) {
+    config.url = config.url.replace('http://localhost:5000', LIVE_BACKEND_URL);
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
